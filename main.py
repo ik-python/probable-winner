5e21d08972c3d0bc59d4959740bc80bb07bb642b
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2022-06-21 21:44:12 +0100

    wip

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ FORMAT = "%(asctime)s %(name)s %(levelname)s - %(message)s"
 logging.basicConfig(format=FORMAT, level=logging.INFO)
 logging.getLogger("winner").setLevel(logging.INFO)
 
-history_file = "assets/history/eu-1537.csv"
+history_file = "assets/history/eu-1538.csv"
 
 
 if __name__ == "__main__":
