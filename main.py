72f7ff7af572dd361a64cca690dc51dd6284e4f4
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2023-03-03 17:29:13 +0000

    scale down
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ FORMAT = "%(asctime)s %(levelname)s - %(message)s"
 logging.basicConfig(format=FORMAT, level=logging.INFO, datefmt="%d-%b-%y %H:%M",)
 logging.getLogger("winner").setLevel(logging.INFO)
 
-history_file = "assets/history/eu-1595.csv"
+history_file = "assets/history/eu-1611.csv"
 
 
 if __name__ == "__main__":
