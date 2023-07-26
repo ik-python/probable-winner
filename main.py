1788f1f7d2d412aa5555ff795bf68c7dfddfca90
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2023-07-26 07:41:36 +0100

    added argparser
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Compute numbers
+## ./main.py --draw 1653
+
+import argparse
+import sys
 import logging
 
 from src.domain.euromillions.rules import Rules
 from src.domain.euromillions.compute import Compute
-
-# import argparse
 
 from src import utils
 
@@ -14,12 +17,18 @@ FORMAT = "%(asctime)s %(levelname)s - %(message)s"
 logging.basicConfig(format=FORMAT, level=logging.INFO, datefmt="%d-%b-%y %H:%M",)
 logging.getLogger("winner").setLevel(logging.INFO)
 
-history_file = "assets/history/eu-1653.csv"
+history_file = "assets/history/eu-{draw}.csv"
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="CLI to helm pick number for Euro Millions", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--draw', type=str, help="Select draw history.")
+    parser.add_argument('--depth', default=6, type=int, help="Choose depth, number of draws.")
+    args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+    logging.info(f'arguments >> {args}')
+
     logging.info("lets check for a winner")
-    numbers = utils.read_csv(file=history_file, depth=9)
+    numbers = utils.read_csv(file=history_file.format(draw=args.draw), depth=args.depth)
     for n in numbers:
         logging.info(n)
 
